aa6787fcf3911bd62002b7bd63854a9dfadcb1c8
Sama Ayman <121590157+Sama-Sokkar@users.noreply.github.com>
2024-05-26 03:33:15 +0300

    Add files via upload

---
+++ src/components/finishScreen.js
@@ -0,0 +1,28 @@
+function finishScreen({ points, maxPoints, highscore, dispatch }) {
+  const percentage = (points / maxPoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "😳";
+  if (percentage >= 0 && percentage < 50) emoji = "😒";
+  if (percentage === 0) emoji = "🤦🏻‍♀️";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+        {maxPoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(HighScore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart the quiz
+      </button>
+    </>
+  );
+}
+
+export default finishScreen;
